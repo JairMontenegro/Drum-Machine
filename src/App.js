6c3e6904e4c19af5c713d103bd2723575091b366
Jair montenegro <jairmontenegrof@gmail.com>
2022-11-04 21:49:53 -0300

    add drumpad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import "./App.css";
+import "./App.scss";
+import DrumPad from "./components/DrumPad";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div id="drum-machine">
+        <div id="display"></div>
+        <DrumPad id="Q">Q</DrumPad>
+        <DrumPad id="W">W</DrumPad>
+        <DrumPad id="E">E</DrumPad>
+        <DrumPad id="A">A</DrumPad>
+        <DrumPad id="S">S</DrumPad>
+        <DrumPad id="D">D</DrumPad>
+        <DrumPad id="Z">Z</DrumPad>
+        <DrumPad id="X">X</DrumPad>
+        <DrumPad id="C">C</DrumPad>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/DrumPad.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DrumPad = (props) => {
+  return <div className="drum-pad">{props.children}</div>;
+};
+
+export default DrumPad;
