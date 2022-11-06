294ff15f8d7ddc521328e043cec41e104aa7690e
Jair montenegro <jairmontenegrof@gmail.com>
2022-11-06 01:44:19 -0300

    past all text

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import { useState } from "react";
 import "./App.scss";
 import DrumPad from "./components/DrumPad";
-import dataDrumPad from "./util/DrumPad-data";
+import dataDrumPad from "./data/DrumPad";
 
 function App() {
+  const [drumKey, setDrumKey] = useState(null);
   return (
     <div className="App">
       <div id="drum-machine">
-        <div id="display"></div>
+        <div id="display">{drumKey}</div>
         {dataDrumPad.map((element) => {
           return (
-            <div key={element.id}>
-              <DrumPad id={element.id} url={element.url}>
+            <button key={element.id} onClick={() => setDrumKey(element.id)}>
+              <DrumPad
+                id={element.id}
+                url={element.url}
+                drumKey={element.drumKey}
+              >
                 {element.drumKey}
               </DrumPad>
-            </div>
+            </button>
           );
         })}
       </div>
--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/DrumPad.scss";
 
 const DrumPad = (props) => {
-  const { id, url } = props;
+  const { id, url, drumKey } = props;
+
+  function handlePlayAudio(key) {
+    document.getElementById(key).play();
+  }
+
+  useEffect(() => {
+    const keyEvent = ({ key }) => {
+      key.toUpperCase() === drumKey && handlePlayAudio(drumKey);
+    };
+
+    window.addEventListener("keypress", keyEvent);
+
+    return () => {
+      window.removeEventListener("keypress", keyEvent);
+    };
+  }, [drumKey]);
+
+  useEffect(() => {
+    const keyEvent = () => {
+      handlePlayAudio(drumKey);
+    };
+
+    window.addEventListener("click", keyEvent);
+
+    return () => {
+      window.removeEventListener("click", keyEvent);
+    };
+  }, [drumKey]);
+
   return (
-    <button className="drum-pad" id={id}>
-      <audio id={props.children.toString()} src={url}></audio>
-      {props.children}
-    </button>
+    <div className="drum-pad" id={id}>
+      <audio className="clip" id={drumKey} src={url}></audio>
+      {drumKey}
+    </div>
   );
 };
 
