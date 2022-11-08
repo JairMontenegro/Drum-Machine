d348764033d19996dbd0f0228bdd09d5f179ac93
Jair montenegro <jairmontenegrof@gmail.com>
2022-11-07 21:34:05 -0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import DrumPad from "./components/DrumPad";
 import dataDrumPad from "./data/DrumPad";
 
 function App() {
-  const [drumKey, setDrumKey] = useState(null);
+  const [drumKey, setDrumKey] = useState("");
+
+  const findId = () => {
+    dataDrumPad.filter((elem) => {
+      return console.log(elem.id);
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", (event) => {
+      start(event.key.toUpperCase(), findId());
+    });
+  }, []);
+
+  const start = (identifier, drumId) => {
+    const audio = document.getElementById(identifier);
+    audio.play();
+    setDrumKey(drumId);
+  };
+
   return (
     <div className="App">
       <div id="drum-machine">
         <div id="display">{drumKey}</div>
-        {dataDrumPad.map((element) => {
-          return (
-            <button key={element.id} onClick={() => setDrumKey(element.id)}>
+        <div className="container">
+          {dataDrumPad.map((element) => {
+            return (
               <DrumPad
+                key={element.id}
                 id={element.id}
                 url={element.url}
                 drumKey={element.drumKey}
+                onClickSound={() => {
+                  start(element.drumKey, element.id);
+                }}
               >
                 {element.drumKey}
               </DrumPad>
-            </button>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,42 +1,12 @@
-import React, { useEffect } from "react";
 import "../styles/DrumPad.scss";
 
 const DrumPad = (props) => {
-  const { id, url, drumKey } = props;
-
-  function handlePlayAudio(key) {
-    document.getElementById(key).play();
-  }
-
-  useEffect(() => {
-    const keyEvent = ({ key }) => {
-      key.toUpperCase() === drumKey && handlePlayAudio(drumKey);
-    };
-
-    window.addEventListener("keypress", keyEvent);
-
-    return () => {
-      window.removeEventListener("keypress", keyEvent);
-    };
-  }, [drumKey]);
-
-  useEffect(() => {
-    const keyEvent = () => {
-      handlePlayAudio(drumKey);
-    };
-
-    window.addEventListener("click", keyEvent);
-
-    return () => {
-      window.removeEventListener("click", keyEvent);
-    };
-  }, [drumKey]);
-
+  const { id, url, drumKey, onClickSound } = props;
   return (
-    <div className="drum-pad" id={id}>
+    <button className="drum-pad" id={id} onClick={onClickSound}>
       <audio className="clip" id={drumKey} src={url}></audio>
       {drumKey}
-    </div>
+    </button>
   );
 };
 
